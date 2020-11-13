e55dbeda675076c989e8de44b7a8f7c924054077
Antonio Leiva <antonioleivag@gmail.com>
2020-11-13 11:30:53 +0100

    App internationalization

--- app/src/main/java/com/antonioleiva/mymovies/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/detail/DetailActivity.kt
@@ -1,10 +1,12 @@
 package com.antonioleiva.mymovies.ui.detail
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
+import com.antonioleiva.mymovies.R
 import com.antonioleiva.mymovies.databinding.ActivityDetailBinding
 import com.antonioleiva.mymovies.model.Movie
 import com.bumptech.glide.Glide
@@ -35,20 +37,19 @@ class DetailActivity : AppCompatActivity() {
 
     private fun bindDetailInfo(detailInfo: TextView, movie: Movie) {
         detailInfo.text = buildSpannedString {
-            bold { append("Original language: ") }
-            appendLine(movie.original_language)
-
-            bold { append("Original title: ") }
-            appendLine(movie.original_title)
-
-            bold { append("Release date: ") }
-            appendLine(movie.release_date)
-
-            bold { append("Popularitiy: ") }
-            appendLine(movie.popularity.toString())
-
-            bold { append("Vote Average: ") }
-            appendLine(movie.vote_average.toString())
+            appendInfo(R.string.original_language, movie.original_language)
+            appendInfo(R.string.original_title, movie.original_title)
+            appendInfo(R.string.release_date, movie.release_date)
+            appendInfo(R.string.popularity, movie.popularity.toString())
+            appendInfo(R.string.vote_average, movie.vote_average.toString())
         }
+    }
+
+    private fun SpannableStringBuilder.appendInfo(stringRes: Int, value: String) {
+        bold {
+            append(getString(stringRes))
+            append(": ")
+        }
+        appendLine(value)
     }
 }
