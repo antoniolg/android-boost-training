4f4b6f66e98557976c0aa0823f9447aed9f51109
Antonio Leiva <antonioleivag@gmail.com>
2020-11-16 14:40:00 +0100

    Ask for Location Permission

--- app/src/main/java/com/antonioleiva/mymovies/model/TheMovieDbService.kt
+++ app/src/main/java/com/antonioleiva/mymovies/model/TheMovieDbService.kt
@@ -6,6 +6,9 @@ import retrofit2.http.Query
 interface TheMovieDbService {
 
     @GET("movie/popular")
-    suspend fun listPopularMovies(@Query("api_key") apiKey: String): MovieDbResult
+    suspend fun listPopularMovies(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String,
+    ): MovieDbResult
 
 }
--- app/src/main/java/com/antonioleiva/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/mymovies/ui/main/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.antonioleiva.mymovies.ui.main
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.antonioleiva.mymovies.R
@@ -9,23 +14,72 @@ import com.antonioleiva.mymovies.databinding.ActivityMainBinding
 import com.antonioleiva.mymovies.model.Movie
 import com.antonioleiva.mymovies.model.MovieDbClient
 import com.antonioleiva.mymovies.ui.detail.DetailActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DEFAULT_REGION = "US"
+    }
+
+    private val moviesAdapter = MoviesAdapter(emptyList()) { navigateTo(it) }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            doRequestPopularMovies(isGranted)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val moviesAdapter = MoviesAdapter(emptyList()) { navigateTo(it) }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         binding.recycler.adapter = moviesAdapter
 
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+    }
+
+    private fun doRequestPopularMovies(isLocationGranted: Boolean) {
         lifecycleScope.launch {
             val apiKey = getString(R.string.api_key)
-            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey)
+            val region = getRegion(isLocationGranted)
+            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey, region)
             moviesAdapter.movies = popularMovies.results
             moviesAdapter.notifyDataSetChanged()
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun getRegion(isLocationGranted: Boolean): String =
+        suspendCancellableCoroutine { continuation ->
+            if (isLocationGranted) {
+                fusedLocationClient.lastLocation.addOnCompleteListener {
+                    continuation.resume(getRegionFromLocation(it.result))
+                }
+            } else {
+                continuation.resume(DEFAULT_REGION)
+            }
+        }
+
+    private fun getRegionFromLocation(location: Location?): String {
+        if (location == null) {
+            return DEFAULT_REGION
+        }
+
+        val geocoder = Geocoder(this)
+        val result = geocoder.getFromLocation(
+            location.latitude,
+            location.longitude,
+            1
+        )
+        return result.firstOrNull()?.countryCode ?: DEFAULT_REGION
     }
 
     private fun navigateTo(movie: Movie) {
